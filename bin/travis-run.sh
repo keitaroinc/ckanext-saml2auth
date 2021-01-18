0524e7d99858718d98cc45c39d7e83176a1fb727
duskobogdanovski <dule.bogdanovski@gmail.com>
2021-01-18 19:11:24 +0100

    Enable coverage

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-nosetests --ckan --with-pylons=subdir/test.ini ckanext/saml2auth/tests
+nosetests --ckan --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext ckanext/saml2auth/tests
 flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan
