26df249805d54fa030ff17a3e6f866066731d97a
amercader <amercadero@gmail.com>
2021-02-17 11:24:03 +0100

    Improve unique name generation
    Only add random digits to the user name if an existing user has the same
    user name, otherwise use the local part of the email.
    
    Move the function to the own extension and add tests.

--- ckanext/saml2auth/views/saml2auth.py
+++ ckanext/saml2auth/views/saml2auth.py
@@ -10,7 +10,6 @@ import ckan.logic as logic
 import ckan.lib.dictization.model_dictize as model_dictize
 from ckan.lib import base
 from ckan.views.user import set_repoze_user
-from ckan.logic.action.create import _get_random_username_from_email
 from ckan.common import config, g, request
 
 from ckanext.saml2auth.spconfig import get_config as sp_config
@@ -88,7 +87,7 @@ def process_user(email, saml_id, firstname, lastname):
             base.abort(400, error_message)
 
     data_dict = {
-        u'name': _get_random_username_from_email(email),
+        u'name': h.ensure_unique_username_from_email(email),
         u'fullname': u'{0} {1}'.format(firstname, lastname),
         u'email': email,
         u'password': h.generate_password(),
