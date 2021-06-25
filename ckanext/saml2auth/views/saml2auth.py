9520671ba13cd75daff4f3330f4f97d8819b0cee
amercader <amercadero@gmail.com>
2021-06-25 14:40:36 +0200

    Allow to customize the fallback route when no RelayState is provided
    If the SAMLResponse doesn't contain a RelayState parameter (a previous
    page that we want to redirect the user to) right now the plugin
    redirects to the user dashboard (the user.me route).
    
    This change keeps this default behaviour but allows to customize the
    route in case fallback redirection to another page is required.

--- ckanext/saml2auth/views/saml2auth.py
+++ ckanext/saml2auth/views/saml2auth.py
@@ -258,7 +258,8 @@ def acs():
 
     relay_state = request.form.get('RelayState')
     redirect_target = toolkit.url_for(
-        relay_state, _external=True) if relay_state else u'user.me'
+        relay_state, _external=True) if relay_state else config.get(
+            'ckanext.saml2auth.default_fallback_endpoint', 'user.me')
 
     resp = toolkit.redirect_to(redirect_target)
 
