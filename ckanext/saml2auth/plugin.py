3a903689630274deb446d8ce229f7412da537305
amercader <amercadero@gmail.com>
2021-07-27 14:12:36 +0200

    [#55] Refactor logout hook so it's easier to test

--- ckanext/saml2auth/plugin.py
+++ ckanext/saml2auth/plugin.py
@@ -103,54 +103,10 @@ class Saml2AuthPlugin(plugins.SingletonPlugin):
 
     def logout(self):
 
-        client = h.saml_client(
-            sp_config()
-        )
-        saml_session_info = get_saml_session_info(session)
-        subject_id = get_subject_id(session)
+        response = _perform_slo()
 
-        if subject_id is None:
-            log.warning(
-                'The session does not contain the subject id for user {}'.format(g.user))
-        else:
-            try:
-                client.users.add_information_about_person(saml_session_info)
-                result = client.global_logout(name_id=subject_id)
-            except LogoutError as e:
-                log.exception(
-                    'SLO not supported by IDP: {}'.format(e))
-                # clear session
-
-            if not result:
-                log.error(
-                    'Looks like the user {} is not logged in any IdP/AA'.format(subject_id))
-
-            if len(result) > 1:
-                log.error(
-                    'Sorry, I do not know how to logout from several sources.'
-                    ' I will logout just from the first one')
-
-            for entity_id, logout_info in result.items():
-                if isinstance(logout_info, tuple):
-                    binding, http_info = logout_info
-                    if binding == entity.BINDING_HTTP_POST:
-                        log.debug(
-                            'Returning form to the IdP to continue the logout process')
-                        body = ''.join(http_info['data'])
-                        extra_vars = {
-                            u'body': body
-                        }
-                        response = base.render(u'saml2auth/idp_logout.html', extra_vars)
-
-                    elif binding == entity.BINDING_HTTP_REDIRECT:
-                        log.debug(
-                            'Redirecting to the IdP to continue the logout process')
-
-                        response = redirect(h.get_location(http_info), code=302)
-                    else:
-                        log.error('Failed to log out from Idp. Unknown binding: {}'.format(binding))
-
-            domain = h.get_site_domain()
+        if response:
+            domain = h.get_site_domain_for_cookie()
 
             # Clear auth cookie in the browser
             response.set_cookie('auth_tkt', domain=domain, expires=0)
@@ -158,4 +114,60 @@ class Saml2AuthPlugin(plugins.SingletonPlugin):
             # Clear session cookie in the browser
             response.set_cookie('ckan', domain=domain, expires=0)
 
-            return response
+        return response
+
+
+def _perform_slo():
+
+    response = None
+
+    client = h.saml_client(
+        sp_config()
+    )
+    saml_session_info = get_saml_session_info(session)
+    subject_id = get_subject_id(session)
+
+    if subject_id is None:
+        log.warning(
+            'The session does not contain the subject id for user {}'.format(g.user))
+        return
+
+    try:
+        client.users.add_information_about_person(saml_session_info)
+        result = client.global_logout(name_id=subject_id)
+    except LogoutError as e:
+        log.exception(
+            'SLO not supported by IDP: {}'.format(e))
+        # clear session
+
+    if not result:
+        log.error(
+            'Looks like the user {} is not logged in any IdP/AA'.format(subject_id))
+
+    if len(result) > 1:
+        log.error(
+            'Sorry, I do not know how to logout from several sources.'
+            ' I will logout just from the first one')
+
+    for entity_id, logout_info in result.items():
+        if isinstance(logout_info, tuple):
+            binding, http_info = logout_info
+            if binding == entity.BINDING_HTTP_POST:
+                log.debug(
+                    'Returning form to the IdP to continue the logout process')
+                body = ''.join(http_info['data'])
+                extra_vars = {
+                    u'body': body
+                }
+                response = base.render(u'saml2auth/idp_logout.html', extra_vars)
+
+            elif binding == entity.BINDING_HTTP_REDIRECT:
+                log.debug(
+                    'Redirecting to the IdP to continue the logout process')
+
+                response = redirect(h.get_location(http_info), code=302)
+            else:
+                log.error(
+                    'Failed to log out from Idp. Unknown binding: {}'.format(binding))
+
+    return response
