91b0b1e133d08955a06c80b9ad852c655180c63d
Pavle Jonoski <pavle.jonoski@gmail.com>
2023-01-25 15:04:59 +0100

    Cleans up the logging in saml2auth.

--- ckanext/saml2auth/views/saml2auth.py
+++ ckanext/saml2auth/views/saml2auth.py
@@ -27,9 +27,7 @@ def get_ckan_user(email):
     ckan_users = model.User.by_email(email)
     if len(ckan_users) > 0:
         ckan_user = ckan_users[0]
-        log.info('CKAN user found: {} for {}'.format(ckan_user, email))
         return ckan_user
-    log.info('CKAN user not found for {}'.format(email))
 
 
 def create_user(context, email, full_name):
@@ -43,7 +41,7 @@ def create_user(context, email, full_name):
 
     try:
         user_dict = logic.get_action(u'user_create')(context, data_dict)
-        log.info('CKAN user created: {}'.format(data_dict['name']))
+        log.debug('CKAN user created: {}'.format(data_dict['name']))
     except logic.ValidationError as e:
         error_message = (e.error_summary or e.message or e.error_dict)
         log.error(error_message)
@@ -82,7 +80,6 @@ def acs():
     u'''The location where the SAML assertion is sent with a HTTP POST.
     This is often referred to as the SAML Assertion Consumer Service (ACS) URL.
     '''
-    log.info('Getting an external redirection')
     g.user = None
     g.userobj = None
 
@@ -103,7 +100,6 @@ def acs():
 
     config_sp = sp_config()
     saml_response = request.form.get(u'SAMLResponse', None)
-    log.info('Validating user with config {} for response {} ...'.format(config_sp, saml_response[:30]))
     client = h.saml_client(config_sp)
     try:
         auth_response = client.parse_authn_request_response(
@@ -126,9 +122,17 @@ def acs():
     # TODO use to connect CKAN user and SAML user
     user_info = auth_response.get_subject()
     saml_id = user_info.text
-    log.info('User info {} SAML id {}'.format(user_info, saml_id))
 
     # Required user attributes for user creation
+    if saml_user_email not in auth_response.ava:
+        log.error(
+            'Cannot find {} in the auth response. ' +
+            'Make sure you have configured ckanext.saml2auth.user_email properly.'.format(
+                saml_user_email
+            ))
+        extra_vars = {u'code': [500], u'content': 'Unable to get user email from identity provider response.'}
+        return base.render(u'error_document_template.html', extra_vars), 500
+
     email = auth_response.ava[saml_user_email][0]
 
     if saml_user_firstname and saml_user_lastname:
@@ -141,26 +145,24 @@ def acs():
         else:
             full_name = u'{} {}'.format(email.split('@')[0], email.split('@')[1])
 
-    log.info('Lookup for ckan user with email: {}'.format(email))
     # Check if CKAN-SAML user exists for the current SAML login
     user = get_ckan_user(email)
 
     if not user:
-        log.info('No such user. Creating new one.')
+        log.debug('No such user. Creating new one.')
         user_dict = create_user(context, email, full_name)
-        log.info('User created.')
         if config.get('ckanext.saml2auth.user_default_org'):
             org_name = config.get('ckanext.saml2auth.user_default_org')
             role = config.get('ckanext.saml2auth.user_default_role', 'member')
             _assign_user_to_organization(user_dict.get('id'), org_name, role)
     else:
         # If account exists and is deleted, reactivate it.
-        log.info('Activating deactivated user.')
+        log.debug('Activating deactivated user.')
         h.activate_user_if_deleted(user)
         user_dict = model_dictize.user_dictize(user, context)
 
     g.user = user_dict['name']
-    log.info('Login with user: {}'.format(g.user))
+    log.debug('Login user')
     # Check if the authenticated user email is in given list of emails
     # and make that user sysadmin and opposite
     h.update_user_sysadmin_status(g.user, email)
@@ -171,12 +173,11 @@ def acs():
     redirect_target = toolkit.url_for(
         str(relay_state), _external=True) if relay_state else u'home.index'
 
-    log.info("Redirecting to: {}".format(redirect_target))
+    log.debug("Redirecting to: {}".format(redirect_target))
     resp = toolkit.redirect_to(redirect_target)
 
     # log the user in programmatically
     set_repoze_user(g.user, resp)
-    log.info('User {} OK, redirecting'.format(g.user))
 
     return resp
 
@@ -193,15 +194,10 @@ def saml2login():
     u'''Redirects the user to the
      configured identity provider for authentication
     '''
-    log.info('Doing SAML2 login...')
+    log.debug('Doing SAML2 login')
     client = h.saml_client(sp_config())
-    log.info('Client: {}'.format(client))
     requested_authn_contexts = get_requested_authn_contexts()
-    log.info('requested_authn_contexts={}'.format(requested_authn_contexts))
-    log.info('toolkit.request={}'.format(toolkit.request))
-    log.info('toolkit.request.args={}'.format(toolkit.request.args))
     relay_state = toolkit.request.args.get('came_from', '')
-    log.info('Client configured. Generating contexts...')
 
     if len(requested_authn_contexts) > 0:
         comparison = config.get('ckanext.saml2auth.requested_authn_context_comparison', 'minimum')
@@ -214,19 +210,17 @@ def saml2login():
             comparison=comparison
         )
 
-        log.info('Preparing to authenticate with auth_context.')
         reqid, info = client.prepare_for_authenticate(requested_authn_context=final_context, relay_state=relay_state)
-        log.info('Prepared OK. Req ID=%s' % reqid)
+        log.debug('Prepared to authenticate with context.')
     else:
-        log.info('Preparing to authenticate without context.')
         reqid, info = client.prepare_for_authenticate(relay_state=relay_state)
-        log.info('Prepared OK. Req ID=%s' % reqid)
+        log.debug('Prepared to authenticate without context.')
 
     redirect_url = None
     for key, value in info[u'headers']:
         if key == u'Location':
             redirect_url = value
-    log.info('Redirecting to: %s' % redirect_url)
+    log.debug('Redirecting to: %s' % redirect_url)
     return toolkit.redirect_to(redirect_url)
 
 
