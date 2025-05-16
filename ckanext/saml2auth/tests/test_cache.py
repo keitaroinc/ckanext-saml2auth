384925c1a90ffbae23ac88719cdcdd8ee9d25529
blagoja <blagojabozinovski@gmail.com>
2025-05-16 19:47:26 +0200

    Tests code refactor

--- ckanext/saml2auth/tests/test_cache.py
+++ ckanext/saml2auth/tests/test_cache.py
@@ -1,4 +1,4 @@
-from ckanext.saml2auth.cache import set_subject_id, get_subject_id
+from ckanext.saml2auth.cache import set_subject_id, get_subject_id, get_saml_session_info
 from types import SimpleNamespace
 from saml2.ident import code
 
@@ -42,3 +42,45 @@ def test_get_subject_id_with_real_code_decode():
     assert result.format == nameid.format
     assert result.text == nameid.text
     assert result.sp_provided_id == nameid.sp_provided_id
+
+
+def test_get_saml_session_info_with_encoded_nameid():
+
+    encoded_nameid = code(nameid)
+    session = {
+        '_saml_session_info': {
+            'name_id': encoded_nameid,
+            'other_data': 'example'
+        }
+    }
+
+    result = get_saml_session_info(session)
+
+    assert isinstance(result['name_id'], object)
+    assert result['name_id'].name_qualifier == nameid.name_qualifier
+    assert result['name_id'].sp_name_qualifier == nameid.sp_name_qualifier
+    assert result['name_id'].text == nameid.text
+    assert result['name_id'].format == nameid.format
+    assert result['name_id'].sp_provided_id == nameid.sp_provided_id
+    assert result['other_data'] == 'example'
+
+
+def test_get_saml_session_info_with_decoded_nameid():
+
+    session = {
+        '_saml_session_info': {
+            'name_id': nameid,
+            'foo': 'bar'
+        }
+    }
+
+    result = get_saml_session_info(session)
+
+    assert result['name_id'] == nameid
+    assert result['foo'] == 'bar'
+
+
+def test_get_saml_session_info_missing():
+    session = {}
+    result = get_saml_session_info(session)
+    assert result is None
