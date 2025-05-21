c15822305fb746c5abec4608b13115535d25dcb3
blagoja <blagojabozinovski@gmail.com>
2025-05-21 10:26:45 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -34,14 +34,14 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.3.5',
+    version='1.4.0',
 
     description='''An extension to enable Single Sign On(SSO) for CKAN data portals via SAML2 Authentication.''',
     long_description=long_description,
     long_description_content_type='text/markdown',
 
     # The project's main homepage.
-    url='https://github.com/okfn/ckanext-saml2auth',
+    url='https://github.com/keitaroinc/ckanext-saml2auth',
 
     # Author details
     author='''Keitaro Inc + OKFN''',
